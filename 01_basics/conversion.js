0566df7b390d0b632831549a7bbe4ca75dec1a91
ShashankLandole <137036653+ShashankLandole@users.noreply.github.com>
2024-03-08 11:38:36 +0000

    conversion in JS

---
+++ 01_basics/conversion.js
@@ -0,0 +1,40 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)//this can be converted into Boolean or String
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/* 
+
+after conversion typeOf is always number
+
+33 => 
+typeOf is => number 
+output is => 33
+
+"33" => 
+typeOf is => String 
+output is => 33
+
+"33abc" => 
+typeOf is => String 
+output is => NaN
+
+"null" => 
+typeOf is => Object 
+output is => 0
+
+"undefined" => 
+typeOf is => undefined 
+output is => NaN
+
+"true" => 
+typeOf is => boolean
+output is => 1
+
+
+
+*/
