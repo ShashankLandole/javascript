415ecfe7e5a8b8cb1f542242c3da1fb895d0b556
ShashankLandole <137036653+ShashankLandole@users.noreply.github.com>
2024-03-09 08:37:06 +0000

    operations

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -12,6 +12,9 @@ console.log(valueInNumber);
 console.log("1" + 2);
 console.log(1 + "2");
 console.log(1 + 2 + "1");
+console.log("1"+1%2);
+console.log("1" + 2/2); 
+console.log(2/2+ "1"); 
 
 /* 
 
